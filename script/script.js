319ebbd3b94ca65f1e70c17c01dd25b467af3802
parkmansoo <qkrakstn02@gmail.com>
2021-04-19 15:33:24 +0900

    20210419_ScrollTop_TopBtn

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $("div#topBtnArea").click(function(){
+        $(window).scrollTop(0);
+    });
+
+    $(window).scroll(function(){
+        // 화면 전체가 스크롤되는 것은 window를 선택자로 지정함.
+
+
+        if($(this).scrollTop() > 300){
+            $("div#topBtnArea").fadeIn(200);
+        } else {
+            $("div#topBtnArea").fadeOut(200);
+        }
+
+    });
+});
